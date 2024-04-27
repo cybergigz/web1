bd0d648c77950b4e3fc971b14af300122ae1054c
Surasak Suksathit <cybergig@gmail.com>
2024-04-27 14:03:17 +0700

    visualize chart area

--- visualize.py
+++ visualize.py
@@ -1,7 +1,8 @@
+import numpy as np
+import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sns
-import streamlit as st
 
 st.title('Visualize')
 ds = st.selectbox('Select Dataset',
@@ -10,6 +11,17 @@ df = sns.load_dataset(ds)
 # select number only
 corr = df.corr(numeric_only=True)
 fig = plt.figure()
-sns.heatmap(corr, annot=True)
+sns.heatmap(corr, annot=corr)
 
 fig
+
+# create self dataframe, thailand lat, long
+lat = np.random.randint(130000, 150000, 100) / 10000
+lon = np.random.randint(1005000, 1010000, 100) / 10000
+df_num = pd.DataFrame(np.vstack([lat,lon]).T,columns=['lat','lon'])
+
+st.area_chart(df_num)
+st.map(df_num)
+st.bar_chart(df_num)
+st.line_chart(df_num)
+st.scatter_chart(df_num)
