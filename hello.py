a2f80992334ae78a5df7b50a36c1135bbc58a7e7
Surasak Suksathit <cybergig@gmail.com>
2024-04-27 11:23:02 +0700

    Iris

--- hello.py
+++ hello.py
@@ -1,6 +1,14 @@
 import streamlit as st
+import pandas as pd
+
+# read df from csv iris dataset from UCI url
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+df = pd.read_csv(url, header=None)
+df.columns = ['sepal_length', 'sepal_width',
+              'petal_length','petal_width','species']
 
 # hello world
 st.title('Hello World!')
-st.write('Ben')
+st.write('Iris dataset')
+st.dataframe(df)
 # test commit
